f188008318fa2be29fe984edc96f831feb9b4527
EthanJones19 <Ethanjones800@yahoo.com>
2021-01-22 11:45:01 -0600

    Added attack function to weapon

--- raygame/Weapon.cpp
+++ raygame/Weapon.cpp
@@ -6,8 +6,18 @@ Weapon::Weapon()
 	m_damage = 1;
 }
 
-Weapon::Weapon(float damage)
+Weapon::Weapon(float damage, float health)
 {
 	m_damage = damage;
 }
 
+void Weapon::attack(Weapon* other)
+{
+	other->takeDamage(getDamage());
+}
+
+float Weapon::takeDamage(float damageAmount)
+{
+	m_health -= damageAmount;
+	return damageAmount;
+}
--- raygame/Weapon.h
+++ raygame/Weapon.h
@@ -1,12 +1,20 @@
 #pragma once
+
 class Weapon
 {
 public:
 	Weapon();
-	Weapon(float damage);
+	Weapon(float damage, float health);
+
+	void attack(Weapon* other);
+	float takeDamage(float damageAmount);
 
 	float getDamage() { return m_damage; }
+	float getHealth() { return m_health; }
+
+
 
 private:
 	float m_damage;
+	float m_health;
 };
